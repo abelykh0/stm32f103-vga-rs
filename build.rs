860ef1ac20b27493c0ce94a68beb12408d522d2c
Andrey Belykh <abelykh@yahoo.ca>
2019-08-07 15:25:25 -0400

    Initial

---
+++ build.rs
@@ -0,0 +1,33 @@
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
+
+fn main() {
+    linker_script_plumbing();
+    build_assembly_sources();
+}
+
+fn build_assembly_sources() {
+    // cc::Build::new()
+    //     .file("src/asm/unpack_1bpp.S")
+    //     .file("src/asm/unpack_text_10p_attributed.S")
+    //     .file("src/asm/copy_words.S")
+    //     .compile("libunrusted.a");
+    // println!("cargo:rerun-if-changed=src/asm/copy_words.S");
+    // println!("cargo:rerun-if-changed=src/asm/unpack_1bpp.S");
+    // println!("cargo:rerun-if-changed=src/asm/unpack_text_10p_attributed.S");
+}
+
+fn linker_script_plumbing() {
+    // Put the linker script somewhere the linker can find it
+    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    File::create(out.join("memory.x"))
+        .unwrap()
+        .write_all(include_bytes!("memory.x"))
+        .unwrap();
+    println!("cargo:rustc-link-search={}", out.display());
+
+    println!("cargo:rerun-if-changed=memory.x");
+    println!("cargo:rerun-if-changed=link-custom.x");
+}
---
+++ src/main.rs
@@ -0,0 +1,99 @@
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+extern crate stm32f1;
+
+use cortex_m_rt::{entry};
+use stm32f1::stm32f103 as device;
+
+#[entry]
+fn main() -> ! {
+    let p = device::Peripherals::take().unwrap();
+
+    configure_clocks(&p.RCC, &p.FLASH);
+    
+    // Configure SysTick
+    let peripherals = cortex_m::peripheral::Peripherals::take().unwrap();
+    let mut syst = peripherals.SYST;
+    syst.set_clock_source(cortex_m::peripheral::syst::SystClkSource::Core);
+    syst.set_reload(1_000);
+    syst.enable_counter();
+
+    // LED is on GPIOC, pin 13
+    p.RCC.apb2enr.modify(|_r, w| w.iopcen().set_bit());
+    p.GPIOC.crh.modify(|_r, w| { w
+        .mode13().output50()
+        .cnf13().push_pull()
+    });
+
+    let mut is_led_on = false;
+    loop {
+        // Delay
+        while !syst.has_wrapped() {}
+
+        // Toggle LED
+        if is_led_on {
+            p.GPIOC.brr.write(|w| w.br13().reset());
+        }
+        else {
+            p.GPIOC.bsrr.write(|w| w.bs13().set());
+        }
+
+        is_led_on = !is_led_on;
+    }
+}
+
+macro_rules! block_while {
+    ($condition:expr) => {
+        while $condition {}
+    };
+}
+
+macro_rules! block_until {
+    ($condition:expr) => {
+        block_while!(!$condition)
+    };
+}
+
+pub fn configure_clocks(rcc: &device::RCC, flash: &device::FLASH) {
+    // Switch to the internal oscillator while messing with the PLL.
+    rcc.cr.modify(|_, w| w.hsion().set_bit());
+    block_until! { rcc.cr.read().hsirdy().bit() }
+
+    // Make the switch.
+    rcc.cfgr.modify(|_, w| w.sw().hsi());
+    block_until! { rcc.cfgr.read().sws().is_hsi() }
+
+    // Turn off the PLL.
+    rcc.cr.modify(|_, w| w.pllon().clear_bit());
+    block_while! { rcc.cr.read().pllrdy().bit() }
+
+    // Apply divisors before boosting frequency.
+    rcc.cfgr.modify(|_, w| { w
+        .hpre().div1()  // AHB
+        .ppre1().div2() // APB1
+        .ppre2().div1() // APB2
+        .usbpre().div1_5()
+    });
+
+    flash.acr.modify(|_, w| w.latency().ws2());
+
+    // Switch on the crystal oscillator.
+    rcc.cr.modify(|_, w| w.hseon().set_bit());
+    block_until! { rcc.cr.read().hserdy().bit() }
+
+    // Configure the PLL.
+    rcc.cfgr.modify(|_, w| { w
+        .pllmul().mul9()
+        .pllsrc().hse_div_prediv()
+    });
+
+    // Turn it on.
+    rcc.cr.modify(|_, w| w.pllon().set_bit());
+    block_until! { rcc.cr.read().pllrdy().bit() }
+
+    // Select PLL as clock source.
+    rcc.cfgr.modify(|_, w| w.sw().pll());
+    block_until! { rcc.cfgr.read().sws() == device::rcc::cfgr::SWSR::PLL }
+}
