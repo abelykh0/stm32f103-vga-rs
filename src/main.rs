bb7e0ca76b0b4a9e04060924498e9748ca935d83
AB <abelykh@yahoo.ca>
2019-11-20 13:59:25 -0500

    Migrating to RTFM 5 (wip)

--- src/main.rs
+++ src/main.rs
@@ -16,83 +16,84 @@ use embedded_graphics::prelude::*;
 use embedded_graphics::fonts::Font6x8;
 use embedded_graphics::primitives::Rectangle;
 use embedded_graphics::pixelcolor::BinaryColor;
-use rtfm::app;
 use stm32f1::stm32f103 as blue_pill;
-use pc_keyboard::KeyEvent;
+//use pc_keyboard::KeyEvent;
 
-#[app(device = stm32f1::stm32f103)]
+#[rtfm::app(device = stm32f1::stm32f103, peripherals = true)]
 const APP: () = {
-    // Late resorce binding
-    static mut GPIOA: blue_pill::GPIOA = ();
-    static mut GPIOC: blue_pill::GPIOC = ();
-    static mut TIM2: blue_pill::TIM2 = ();
-    static mut TIM3: blue_pill::TIM3 = ();
-    static mut TIM4: blue_pill::TIM4 = ();
+    struct Resources {
+        gpioa: blue_pill::GPIOA,
+        gpioc: blue_pill::GPIOC,
+        tim2: blue_pill::TIM2,
+        tim3: blue_pill::TIM3,
+        tim4: blue_pill::TIM4,
+        keyboard : Ps2Keyboard,
+        vga_draw : VgaDraw,
+        display : VgaDisplay
+    }
 
-    // VGA
-    static mut DISPLAY : VgaDisplay = VgaDisplay {
-        pixels : [0; (vga::HSIZE_CHARS * 8 * vga::VSIZE_CHARS) as usize],
-        attributes : [0; (vga::HSIZE_CHARS * vga::VSIZE_CHARS) as usize],
-        attribute_definitions : [0; 320]
-    };
-    static mut VGA_DRAW : VgaDraw = VgaDraw::new();
-
-    // PS/2 Keyboard
-    static mut KEYBOARD : Ps2Keyboard = ();
-
-    #[init (resources = [DISPLAY, VGA_DRAW])]
-    fn init() -> init::LateResources {
+    #[init]
+    fn init(cx: init::Context) -> init::LateResources {
         // Configure PLL and flash
-        stm32::configure_clocks(&device.RCC, &device.FLASH);
+        stm32::configure_clocks(&cx.device.RCC, &cx.device.FLASH);
 
         // Configures the system timer to trigger a SysTick exception every second
         //stm32::configure_systick(&mut core.SYST, 72_000); // period = 1ms
 
         // Built-in LED is on GPIOC, pin 13
-        device.RCC.apb2enr.modify(|_r, w| w.iopcen().set_bit());
-        device.GPIOC.crh.modify(|_r, w| { w
+        cx.device.RCC.apb2enr.modify(|_r, w| w.iopcen().set_bit());
+        cx.device.GPIOC.crh.modify(|_r, w| { w
             .mode13().output50()
             .cnf13().push_pull()
         });
 
+        // Initialize VGA
+        let mut display = VgaDisplay {
+            pixels : [0; (vga::HSIZE_CHARS * 8 * vga::VSIZE_CHARS) as usize],
+            attributes : [0; (vga::HSIZE_CHARS * vga::VSIZE_CHARS) as usize],
+            attribute_definitions : [0; 320]
+        };
+
         // This is used to display 64 colors
         for i in 0..64 {
             for j in 0..4 {
-                resources.DISPLAY.attribute_definitions[(i << 2) + 64 + j] = convert_color(i as u8);
+                display.attribute_definitions[(i << 2) + 64 + j] = convert_color(i as u8);
             }
         }
-
-        // Initialize VGA
-        resources.DISPLAY.init_default_attribute(convert_color(0x10), convert_color(0x3F));
-        resources.VGA_DRAW.init(&resources.DISPLAY);
-        vga::render::init_vga(&device);
+        display.init_default_attribute(convert_color(0x10), convert_color(0x3F));
+        let mut vga_draw = VgaDraw::new();
+        vga_draw.init(&display);
+        vga::render::init_vga(&cx.device);
 
         // Initialize keyboard
-        Ps2Keyboard::init(&device);
+        Ps2Keyboard::init(&cx.device);
 
         init::LateResources { 
-            GPIOA: device.GPIOA,
-            GPIOC: device.GPIOC,
-            TIM2: device.TIM2,
-            TIM3: device.TIM3,
-            TIM4: device.TIM4,
-            KEYBOARD : Ps2Keyboard::new()
+            gpioa: cx.device.GPIOA,
+            gpioc: cx.device.GPIOC,
+            tim2: cx.device.TIM2,
+            tim3: cx.device.TIM3,
+            tim4: cx.device.TIM4,
+            keyboard : Ps2Keyboard::new(),
+            vga_draw : vga_draw,
+            display : display
         }
     }
 
-    #[idle (resources = [TIM2, TIM4, DISPLAY, GPIOC])]
-    fn idle() -> ! {
-        resources.DISPLAY.draw(
+    #[idle (resources = [tim2, tim4, display, gpioc])]
+    fn idle(mut cx: idle::Context) -> ! {
+        /*
+        cx.resources.display.draw(
             Rectangle::new(Point::new(2, 2), Point::new(vga::HSIZE_CHARS as i32 * 8 - 3, vga::VSIZE_CHARS as i32 * 8 - 3)).stroke(Some(BinaryColor::On))
         );
-        resources.DISPLAY.draw(
+        cx.resources.display.draw(
             Rectangle::new(Point::new(4, 4), Point::new(vga::HSIZE_CHARS as i32 * 8 - 5, vga::VSIZE_CHARS as i32 * 8 - 5)).stroke(Some(BinaryColor::On))
         );
 
         for i in 0..64 {
             let mut buffer = [0u8; 6];
             let color = format_color(i as u8, &mut buffer);
-            resources.DISPLAY.draw(
+            cx.resources.display.draw(
                 Font6x8::render_str(color)
                 .stroke(Some(BinaryColor::On))
                 .translate(Point::new(16 + (i % 6) * 56, 41 + (i / 6) * 16))
@@ -105,49 +106,51 @@ const APP: () = {
             offset2 |= offset2 << 4;
             for j in 0..5 {
                 for y in 0..8 {
-                    resources.DISPLAY.pixels[(y_position * 8 + y) * vga::HSIZE_CHARS as usize + x_position + j as usize] = offset2;
+                    cx.resources.display.pixels[(y_position * 8 + y) * vga::HSIZE_CHARS as usize + x_position + j as usize] = offset2;
                 }
-                resources.DISPLAY.attributes[y_position * vga::HSIZE_CHARS as usize + x_position + j as usize] = offset1;
+                cx.resources.display.attributes[y_position * vga::HSIZE_CHARS as usize + x_position + j as usize] = offset1;
             }
         }
+        */
 
         loop {
         }
     }
 
-    #[exception (resources = [GPIOA, KEYBOARD])]
-    fn PendSV() {
-        let gpio_bits = resources.GPIOA.idr.read().bits() as u16;
-        resources.KEYBOARD.update(gpio_bits);
+    #[task(binds = PendSV, resources = [gpioa, keyboard])]
+    fn pendSV(cx: pendSV::Context) {
+        let gpio_bits = cx.resources.gpioa.idr.read().bits() as u16;
+        cx.resources.keyboard.update(gpio_bits);
     }
 
-    #[interrupt (priority = 16, resources = [TIM4, VGA_DRAW])]
-    fn TIM4() 
+    #[task(binds = TIM3, priority = 16, resources = [tim3, vga_draw])]
+    fn tim3(cx: tim3::Context) 
     {
         // Acknowledge IRQ
-        resources.TIM4.sr.modify(|_, w| w.cc4if().clear_bit());
+        cx.resources.tim3.sr.modify(|_, w| w.cc2if().clear_bit());
 
-        resources.VGA_DRAW.on_vsync();
+        cx.resources.vga_draw.on_hsync();
     }
 
-    #[interrupt (priority = 16, resources = [TIM3, VGA_DRAW])]
-    fn TIM3() 
+    #[task(binds = TIM4, priority = 16, resources = [tim4, vga_draw])]
+    fn tim4(cx: tim4::Context) 
     {
         // Acknowledge IRQ
-        resources.TIM3.sr.modify(|_, w| w.cc2if().clear_bit());
+        cx.resources.tim4.sr.modify(|_, w| w.cc4if().clear_bit());
 
-        resources.VGA_DRAW.on_hsync();
+        cx.resources.vga_draw.on_vsync();
     }
 
-    #[interrupt (priority = 15, resources = [TIM2])]
-    fn TIM2() 
+    #[task(binds = TIM2, priority = 15, resources = [tim2])]
+    fn tim2(cx: tim2::Context) 
     {
         // Acknowledge IRQ
-        resources.TIM2.sr.modify(|_, w| w.cc2if().clear_bit());
+        cx.resources.tim2.sr.modify(|_, w| w.cc2if().clear_bit());
 
         // Idle the CPU until an interrupt arrives
         cortex_m::asm::wfi();
     }
+
 };
 
 fn format_color(color : u8, buffer: &mut [u8]) -> &str {
